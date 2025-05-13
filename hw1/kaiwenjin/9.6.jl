91236171d6acd0a818f37bd35d303d245832eedc
Yui <2946723935@qq.com>
2025-05-13 15:21:44 +0800

    hw1 update

--- hw1/kaiwenjin/9.6.jl
+++ hw1/kaiwenjin/9.6.jl
@@ -1,11 +1,11 @@
 using Test
-function pp_dp(w::Vector{Int}, v::Vector{Int}, V::Int)
-    @assert findmin(w)[1] >= 0 && findmin(v)[1] >= 0 && V >= 0
+function pp_dp(w::Vector{Int}, v::Vector{Int}, W::Int)
+    @assert findmin(w)[1] >= 0 && findmin(v)[1] >= 0 && W >= 0
     @assert length(w) == length(v) 
     n = length(w)
-    dp = zeros(Int, n+1, V+1) 
+    dp = zeros(Int, n+1, W+1) 
     for i in 1:n
-        for j in 0:V
+        for j in 0:W
             if j < w[i]
                 dp[i+1, j+1] = dp[i, j+1]
             else
@@ -13,12 +13,12 @@ function pp_dp(w::Vector{Int}, v::Vector{Int}, V::Int)
             end
         end
     end
-    return dp[n+1, V+1]
+    return dp[n+1, W+1]
 end
 
-function pp(w::Vector{Int}, v::Vector{Int}, V::Int)
+function pp(w::Vector{Int}, v::Vector{Int}, W::Int)
     @assert length(w) == length(v)
-    @assert findmin(w)[1] >= 0 && findmin(v)[1] >= 0 && V >= 0
+    @assert findmin(w)[1] >= 0 && findmin(v)[1] >= 0 && W >= 0
     n = length(w)
     res2 = -1
     for k = 0:2^n-1
@@ -28,7 +28,7 @@ function pp(w::Vector{Int}, v::Vector{Int}, V::Int)
             k >>= 1
         end
         val = sum(chosen .* v)
-        if val > res2 && sum(chosen .* w) <= V
+        if val > res2 && sum(chosen .* w) <= W
             res2 = val
         end
     end
@@ -37,8 +37,8 @@ end
 @testset "package_problem" begin
     w = [2,3,4,7]
     v = [1,3,5,9]
-    V = 10
-    res1 = pp_dp(w,v,V)
-    res2 = pp(w,v,V)
+    W = 10
+    res1 = pp_dp(w,v,W)
+    res2 = pp(w,v,W)
     @test res1 == res2
 end
