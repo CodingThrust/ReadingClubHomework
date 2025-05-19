cf4fccfd20c5448941ffbdb77cbcda33d159005c
Yusheng Zhao <yushengzhao2020@outlook.com>
2025-05-19 21:22:38 +0800

    pkplplplplpl

--- hw2/YushengZhao/9_26.jl
+++ hw2/YushengZhao/9_26.jl
@@ -3,3 +3,5 @@
 # on the other hand if the formula is not satisfied, the bound of at least one y_i is 1/2 - y_0/3 <= y_i <= 1/2 +y_0, and the min f(x) is  num unsat * y_0^2 - y_0^2/9 + sum_(sat clause) (y_0^2 - y_0^2) - y_0^2/2 + num_unsat * y_0^ / 2*n*9 which is at least 7/18 y_0^2 
 
 # using hint, if formula is satisfied, y^* cannot be local minimum because can slide over y_0 axis a little bit, epsilon, and get lower. this is in the neighborhood of y^* because y_i' 's difference from y_i = 1/2 is bounded by the epsilon. And f(x)'s min is -y_0^2/2 when formula is satisfied. This min is smaller than f(y^*) = 0. 
+
+# so x_i means is y_i >= 1/2 - y_0/3 ?
